ced538e6764bfd37f7bc9d79f213fa35fdd2a145
michellekenia <keniamsoaress@gmail.com>
2024-07-30 23:38:11 -0300

    implementar-rotas

--- src/conta/conta.controller.ts
+++ src/conta/conta.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
+import { ContaService } from './conta.service';
+import { Conta, TipoConta } from './conta.model';
 
 @Controller('conta')
-export class ContaController {}
+export class ContaController {
+
+    constructor(private readonly contaService: ContaService) { }
+
+    @Post()
+    criarConta(@Body('clienteId') clienteId: number, @Body('saldo') saldo: number, @Body('tipo') tipo: TipoConta): Conta {
+        return this.contaService.criarConta(clienteId, saldo, tipo)
+     }
+
+
+    @Get()
+    findAll(): Conta[] {
+        return this.contaService.findAll()
+    }
+
+    @Get(':id')
+    findById(@Param('id') id: number): Conta {
+        return this.contaService.findById(id)
+    }
+
+    @Patch(':id/conta')
+    alterarTipoConta(@Param('id') id: number, @Body('tipoConta') tipoConta: TipoConta): Conta {
+        return this.contaService.alterarTipoConta(id, tipoConta)
+    }
+
+    @Patch(':id/saldo')
+    alterarSaldo(@Param('id') id: number, @Body('saldo') novoSaldo: number): Conta {
+        return this.contaService.alterarSaldo(id, novoSaldo)
+    }
+
+    @Delete(':id')
+    removerconta(@Param('id', ParseIntPipe) id: number): void {
+        return this.contaService.removerConta(id)
+    }
+
+}
