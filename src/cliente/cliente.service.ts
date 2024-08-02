e9751c496d4765d21c0a34f245aaa9b6ecf8c282
michellekenia <keniamsoaress@gmail.com>
2024-08-02 12:31:18 -0300

    criar-rotas

--- src/cliente/cliente.service.ts
+++ src/cliente/cliente.service.ts
@@ -1,4 +1,82 @@
-import { Injectable } from '@nestjs/common';
+import { Cliente } from 'src/cliente/cliente.model';
+import { Injectable, NotFoundException, Post } from '@nestjs/common';
+import * as path from 'path';
+import * as fs from 'fs';
+import { Conta } from 'src/conta/conta.model';
 
 @Injectable()
-export class ClienteService {}
+export class ClienteService {
+    private readonly filePath = path.resolve('src/cliente/clientes.json')
+
+    private lerClientes(): Cliente[] {
+        const data = fs.readFileSync(this.filePath, 'utf8');
+        return JSON.parse(data) as Cliente[];
+    }
+
+
+    private escreverCliente(clients: Cliente[]): void {
+        fs.writeFileSync(this.filePath, JSON.stringify(clients, null, 2), 'utf8');
+    }
+
+    criarCliente(nome: string, endereco: string, telefone: string, gerenteId: number, contas: Conta[] = []): Cliente {
+        const clientes = this.lerClientes()
+        const novoCliente: Cliente = {
+            clienteId:
+                clientes.length > 0 ? clientes[clientes.length - 1].clienteId + 1 : 1,
+            nome,
+            endereco,
+            telefone,
+            gerenteId,
+            contas
+        }
+
+        clientes.push(novoCliente)
+        this.escreverCliente(clientes)
+        return novoCliente
+
+    }
+
+    findAll(): Cliente[] {
+        return this.lerClientes()
+    }
+
+    findById(id: number): Cliente {
+        const clientes = this.lerClientes()
+        const cliente = clientes.find((cliente) => cliente.clienteId === Number(id))
+        if (!cliente) {
+            throw new NotFoundException(`Cliente de id ${id} não encontrado.`)
+        }
+        return cliente
+    }
+
+    alterarDadosClinte(id: number, novoNome: string, novoEndereco: string, novoTelefone: string): Cliente {
+        const clientes = this.lerClientes()
+        const cliente = clientes.find(cliente => cliente.clienteId === Number(id))
+
+        if (!cliente) {
+            throw new NotFoundException('Cliente não encontrado.')
+        }
+
+        cliente.nome = novoNome,
+            cliente.endereco = novoEndereco,
+            cliente.telefone = novoTelefone
+
+        this.escreverCliente(clientes)
+        return cliente
+
+    }
+
+    removerCliente(id: number): void {
+        const clientes = this.lerClientes()
+        const clientesIndex = clientes.findIndex(cliente => cliente.clienteId === Number(id))
+
+        if (clientesIndex < 0) {
+            throw new NotFoundException('Cliente não encontrado.')
+        }
+
+        clientes.splice(clientesIndex)
+        this.escreverCliente(clientes)
+    }
+
+}
+
